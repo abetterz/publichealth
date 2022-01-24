32b0cb41773b417e9d3a0a0fe1d4af75dd910e5b
lesly <leslyrevenge@gmail.com>
2022-01-24 12:09:36 -0500

    added link to front page

--- src/components/screens/archives/config.js
+++ src/components/screens/archives/config.js
@@ -3,12 +3,14 @@ import reports from "./report";
 import video_archives from "./video_archives";
 import covid_boot_camp from "./covid_boot_camp";
 import scientific_studies from "./scientific_studies";
+import covid_19_overview from "./covid_19_overview";
 const dict = {
   bootcamp_for_parents,
   reports,
   video_archives,
   covid_boot_camp,
   scientific_studies,
+  covid_19_overview,
 };
 
 const GetPage = (dict_key) => {
---
+++ src/components/screens/archives/covid_19_overview.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+export const Covid19Overview = (props) => {
+  return <div> covid_19_overview</div>;
+};
+
+const mapStateToProps = (state) => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Covid19Overview);
