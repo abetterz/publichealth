6583e7c4def34c07a83ca35bd49c22bb4b57ec4b
lesly <leslyrevenge@gmail.com>
2022-02-07 22:38:18 -0500

    remove items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.scss";
 import Screens from "./components/screens";
-import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
-import { Row, Col } from "antd";
 import AppRoute from "./app/route";
 function App() {
   return <Screens />;
