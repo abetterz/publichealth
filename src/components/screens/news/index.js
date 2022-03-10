5872040037740b4b84bae4356d0a71776b6a2c78
ompatel-champion <ompatelupwork@gmail.com>
2022-03-10 21:23:49 +0800

    test

--- src/components/screens/news/index.js
+++ src/components/screens/news/index.js
@@ -238,7 +238,7 @@ const News = (props) => {
 
   let { section } = useParams();
 
-  console.log(props.recent_news[0], "checking_for_exclusive");
+  console.log(props.recent_news, "checking_for_exclusive");
 
   let span = {
     left: {
@@ -284,102 +284,7 @@ const News = (props) => {
 
   const getTitle = title[section] || title.default;
 
-  const doctors = [
-    {
-      name: "Paul Elias Alexander, PhD",
-      link: "https://substack.com/profile/58916651-dr-paul-alexander",
-      image:
-        "https://childrenshealthdefense.org/wp-content/uploads/PaulAlexander.jpg",
-      subtitle:
-        "Paul Elias Alexander is a Canadian health researcher and a former Trump administration official ",
-    },
-    {
-      name: "Ryan N. Cole MD",
-      link: "https://independentdocsid.com/RyanColeMD",
-      image:
-        "https://bloximages.chicago2.vip.townnews.com/idahopress.com/content/tncms/assets/v3/editorial/c/2e/c2ee54ab-3c0d-522d-9d4c-2bfd94d5d87e/6111c36242d7c.image.jpg?crop=889%2C889%2C0%2C195&resize=1200%2C1200&order=crop%2Cresize",
-      subtitle:
-        "Dr. Cole is a board-certified dermatopathologist (AP & CP) and the CEO/Medical Director of Cole Diagnostics. ",
-    },
-    {
-      link: "https://www.rwmalonemd.com/",
-      name: "Robert W Malone MD",
-
-      image:
-        "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/aqiwr1hm17sm1glcsvhz",
-      subtitle: "American virologist and immunologist",
-    },
-  ];
-
-  const domains = [
-    {
-      name: "Front Line COVID-19 Critical Care Alliance",
-      link: "https://covid19criticalcare.com/",
-      image:
-        "https://covid19criticalcare.com/wp-content/uploads/2020/06/FLCCC_Alliance_footer.svg",
-      subtitle:
-        "Formed by leading critical care specialists in March 2020, at the beginning of the Coronavirus pandemic",
-    },
-    {
-      name: "America’s Frontline Doctors",
-      link: "https://americasfrontlinedoctors.org/news/",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/c/c1/America%27s_Frontline_Doctors_official_logo.png",
-      subtitle: "Fighting for Your Medical Freedom.",
-    },
-    {
-      name: "C19 Early",
-
-      subtitle:
-        "Treatments do not replace vaccines and other measures. All practical, effective, and safe means should be used. Elimination is a race against viral evolution.",
-    },
-  ];
-
-  const alexander_substack = [
-    {
-      subtitle:
-        "Supreme Court’s Gorsuch refused to wear mask despite request over Sotomayor’s Covid concerns, report says despite a request from Chief Justice John Roberts for all members of the high court to wear",
-      image:
-        "https://bucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com/public/images/2b69936a-c043-4317-b71a-b84867777535_2800x2800.png",
-    },
-    {
-      subtitle:
-        "It was NEVER COVID the 'virus', it was the 'RESPONSE' to it, the corrupted, POLITICAL response, not public health, oh I supported and loved Trump, to me outstanding, but made 2 catastrophic mistakes ",
-      image:
-        "https://bucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com/public/images/2b69936a-c043-4317-b71a-b84867777535_2800x2800.png",
-    },
-    {
-      subtitle:
-        "Israeli vaccine chief: “We have made mistakes”; finally a public health official/scientist, accepting responsibility & saying they were wrong; Covid will be “like flu”...big praise for honesty ",
-      image:
-        "https://bucketeer-e05bbc84-baa3-437e-9518-adb32be77984.s3.amazonaws.com/public/images/2b69936a-c043-4317-b71a-b84867777535_2800x2800.png",
-    },
-  ];
-
-  const science_substack = [
-    {
-      subtitle:
-        "The “original antigenic sin” and its relevance for SARS-CoV-2 (COVID-19) vaccination ",
-      name: "ScienceDirect",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/thumb/6/6a/Elsevier_logo_2019.svg/1200px-Elsevier_logo_2019.svg.png",
-    },
-    {
-      subtitle:
-        "Reconciling estimates of global spread and infection fatality rates of COVID-19: An overview of systematic evaluations ",
-      name: "Wiley Online Library",
-      image:
-        "https://library.stlawu.edu/sites/default/files/2020-04/wiley_online_clr2.png",
-    },
-    {
-      subtitle: "Cribriform Plate Injury After Nasal Swab Testing for COVID-19",
-      name: "JAMA Network",
-      image:
-        "https://publons.com/media/thumbs/publishers/logos/fbfd45e4-834b-40ca-b947-c871cdcea0d8.png.200x200_q95_detail_letterbox_upscale.png",
-    },
-  ];
-
-  console.log(props.recent_news[0]);
+  console.log(props.recent_news);
   return (
     <Row gutter={24}>
       <Col {...span.left}>
