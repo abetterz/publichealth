f00e6d1336671bbf8d829d092325e01a59a82b2f
Wilkens <wilkenslavarin@gmail.com>
2022-04-07 18:18:41 -0400

    Analytics Changed

--- src/components/screens/home/rightSide.js
+++ src/components/screens/home/rightSide.js
@@ -28,13 +28,13 @@ export default function RightSide(props) {
           </Divider>
         </Col>
         <Col span={24}>{props.children}</Col>
-        {!hide_see_more && (
+        {/*!hide_see_more && (
           <Col onClick={onClick} span={24}>
             {/*<a href={link_to}>
               <div className="load_more">See More</div>
-        </a>*/}
+        </a>}
           </Col>
-        )}
+        )*/}
       </Row>
     );
   }
